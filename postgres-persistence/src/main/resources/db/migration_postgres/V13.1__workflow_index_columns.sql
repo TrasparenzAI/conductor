2a1d2a0e20af231b5736b27517696d696113c03b
marco.spasiano <marco.spasiano@cnr.it>
2025-03-24 10:05:03 +0100

    new feature: add index on workflow json_data

--- postgres-persistence/src/main/resources/db/migration_postgres/V13.1__workflow_index_columns.sql
+++ postgres-persistence/src/main/resources/db/migration_postgres/V13.1__workflow_index_columns.sql
@@ -4,3 +4,10 @@ ALTER TABLE workflow_index
 -- SET DEFAULT AGAIN IN CASE COLUMN ALREADY EXISTED from deleted V13 migration
 ALTER TABLE workflow_index
     ALTER COLUMN update_time SET DEFAULT TIMESTAMP WITH TIME ZONE 'epoch';
+
+
+CREATE EXTENSION pg_trgm;
+
+DROP INDEX IF EXISTS workflow_json_data_idx;
+
+CREATE INDEX workflow_json_data_idx ON workflow USING GIN (json_data gin_trgm_ops);
